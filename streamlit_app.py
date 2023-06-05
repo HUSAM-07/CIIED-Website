7997b142fe86f840dc4b7034e5a0077ecd88b34b
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-05 21:32:51 +0400

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,45 @@
 import streamlit as st
-import pandas as pd
 
-# Load the data
-df = pd.read_csv('founders_resources.csv')
+# Sample data
+websites = {
+    'Productivity': [
+        {'name': 'Trello', 'url': 'https://trello.com'},
+        {'name': 'Asana', 'url': 'https://asana.com'},
+        {'name': 'Notion', 'url': 'https://www.notion.so'},
+    ],
+    'Design': [
+        {'name': 'Figma', 'url': 'https://www.figma.com'},
+        {'name': 'Sketch', 'url': 'https://www.sketch.com'},
+        {'name': 'Canva', 'url': 'https://www.canva.com'},
+    ],
+    'Marketing': [
+        {'name': 'HubSpot', 'url': 'https://www.hubspot.com'},
+        {'name': 'Google Analytics', 'url': 'https://analytics.google.com'},
+        {'name': 'Mailchimp', 'url': 'https://mailchimp.com'},
+    ],
+}
 
-# Create a search bar
-search_bar = st.text_input('Search for a website:')
+# Sidebar - Categories
+categories = list(websites.keys())
+selected_category = st.sidebar.selectbox('Select a category', ['All'] + categories)
 
-# Search for the website
-if search_bar:
-    results = df[df['Website'].str.contains(search_bar)]
+# Search bar
+search_query = st.sidebar.text_input('Search for a website')
 
-    # Display the results
-    if results.empty:
-        st.write('No results found.')
-    else:
-        st.table(results)
+# Filter websites based on category and search query
+filtered_websites = []
+for category, websites_list in websites.items():
+    if selected_category == 'All' or selected_category == category:
+        if search_query:
+            filtered_websites += [website for website in websites_list if search_query.lower() in website['name'].lower()]
+        else:
+            filtered_websites += websites_list
 
-# Display the categories
-categories = df['Category'].unique()
+# Display filtered websites
+if filtered_websites:
+    st.write(f"Found {len(filtered_websites)} websites")
+    for website in filtered_websites:
+        st.write(f"- [{website['name']}]({website['url']})")
+else:
+    st.write("No websites found")
 
-# Create a dropdown menu for the categories
-category_dropdown = st.selectbox('Select a category:', categories)
-
-# Filter the data by category
-if category_dropdown:
-    results = df[df['Category'] == category_dropdown]
-
-    # Display the results
-    if results.empty:
-        st.write('No results found.')
-    else:
-        st.table(results)
